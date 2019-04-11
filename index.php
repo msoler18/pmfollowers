0a2ad8bae0371832fb85401ea2d3c6a6249ea3e7
msoler18 <msoler735@gmail.com>
2019-04-11 15:23:01 -0500

    Add index for plugin

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name:  Planet Meetup Autor Followers
+  Version: 1.0
+  Plugin URI: https://gradiweb.com
+  Description: Followers system for Planet Meetup Web App
+  Author URI: https://github.com/msoler18/
+  Author: msoler18
+  License: GPL2
+ */
+?>
